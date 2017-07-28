d7848b03e2490e093b3639ffb029d2f2728c9793
Matthew Holman <mateoholman@gmail.com>
2017-07-28 13:47:46 -0500

    Modified the App and App test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,9 +4,18 @@ import { shallow } from 'enzyme';
 
 import App from './components/App';
 
+//Test what it renders
+//Test state & props the component receives
+//Test what the component does when the user interacts with it (via clicking, dragging, keyboard input, etc)
+
 describe('<App />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+
+  it('should have a header');
+
+  it('should have a footer');
+
 });
