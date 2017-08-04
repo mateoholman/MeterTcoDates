d58e32da5a0871a8081a77967f254d4b39153597
Matthew Holman <mateoholman@gmail.com>
2017-08-03 22:09:52 -0500

    Setup redux, redux store and redux dev tools

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # To-do
-1. Setup the AdminPanel componet.
+1. Map out state for redux
+2. Setup redux store
+3. Setup action creators
+4. Setup reducers
+5. Tie all the redux stuff together
 
 # Process
 1. Scaffold frontend with create-react-app
@@ -10,3 +14,5 @@
 6. Build the table component.
 7. Scaffold the AdminPanel
 8. Create a data file for the JSON data
+9. Install redux, react-redux, redux-promise & redux-devtools-extension
+10. Setup the redux store, middleware, and devtools extension in the App.
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import promise from 'redux-promise';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
 
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(promise)));
+
 ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
   ), document.getElementById('root')
   );
 registerServiceWorker();
