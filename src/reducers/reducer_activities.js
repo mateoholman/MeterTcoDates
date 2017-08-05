17080cac9150559e726e28d52318ea5012e17a32
Matthew Holman <mateoholman@gmail.com>
2017-08-04 22:02:23 -0500

    Reducer now pulls-in JSON data

--- src/reducers/reducer_activities.js
+++ src/reducers/reducer_activities.js
@@ -1,21 +1,5 @@
-
+import activityData from '../data/activities.json';
 
 export default function() {
-  return [
-    {
-      "_id": "1",
-      "activity": "WW Line Installation Complete",
-      "duration": 30
-    },
-    {
-      "_id": "2",
-      "activity": "Schedule City Testing & Inspections",
-      "duration": 7
-    },
-    {
-      "_id": "3",
-      "activity": "City Testing &  Inspections",
-      "duration": 14
-    }
-  ]
+  return activityData;
 }
