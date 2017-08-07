ab3c18551c66fee709c971b9b5be3878804445c0
Matthew Holman <mateoholman@gmail.com>
2017-08-07 15:23:42 -0500

    Refactored the multiselect to connect to redux-form

--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -6,37 +6,29 @@ import '../styles/ProjectForm.css';
 
 class ProjectForm extends Component {
 
-  renderProjectType(field){
-    return(
-      <Row>
-        <FormGroup controlId="projectType">
-          <Col smOffset={2} componentClass={ControlLabel} sm={2}>
-            Project Type:
-          </Col>
-          <Col sm={4}>
-            <FormControl componentClass="select" placeholder="DTRes">
-              <option value="DTRes">Downtown Residential / Hotel</option>
-              <option value="DTOffice">Downtown Office</option>
-              <option value="Multifamily">Multifamily</option>
-              <option value="OfficeWarehouse">Office / Warehouse</option>
-            </FormControl>
-          </Col>
-        </FormGroup>
-      </Row>
-
-    );
-  }
-
   render() {
     return (
       <div className="project-form">
         <h2>Choose The Type of Project</h2>
 
         <Form>
-            <Field
-              name="projectType"
-              component={this.renderProjectType}
-            />
+          
+          <Row>
+            <FormGroup controlId="projectType">
+              <Col smOffset={2} componentClass={ControlLabel} sm={2}>
+                Project Type:
+              </Col>
+              <Col sm={4}>
+                <Field name="projectType" component="select">
+                  <option value="DTRes">Downtown Residential / Hotel</option>
+                  <option value="DTOffice">Downtown Office</option>
+                  <option value="Multifamily">Multifamily</option>
+                  <option value="OfficeWarehouse">Office / Warehouse</option>
+                </Field>
+              </Col>
+            </FormGroup>
+          </Row>
+
           <Row>
             <FormGroup controlId="scheduleType">
               <Col smOffset={2} componentClass={ControlLabel} sm={2}>
