d8cd6ae9b2c4fee92ac3e76b5dec67a4e8647dee
Matthew Holman <mateoholman@gmail.com>
2017-09-05 16:23:58 -0500

    Production build updates

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
@@ -12,6 +14,16 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
+app.all('*', (req, res, next) => {
+  req.status = 401;
+  return res.json(`Not found. Couldn't find it. It's not here!`);
+});
+
+app.use((err, req, res, next) => {
+  res.status = 500;
+  return res.json(`Unable to process request. ERROR: ${err}`);
+});
 
 const PORT = process.env.PORT || 5000;
 
